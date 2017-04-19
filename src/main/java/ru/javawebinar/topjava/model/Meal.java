ca40715f83320dd73516e858771e42ff0fa5978e
Tolegen Izbassar <tolegen.izbasar@samgau.com>
2017-04-19 18:03:41 +0600

    HW4 impl

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,7 +1,18 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -10,11 +21,71 @@ import java.time.LocalTime;
  * GKislin
  * 11.01.2015.
  */
+@Entity
+@NamedQueries({
+        @NamedQuery(name = Meal.FindByIdAndUserId.NAME,
+                    query = Meal.FindByIdAndUserId.QUERY),
+        @NamedQuery(name = Meal.FindByUserId.NAME,
+                    query = Meal.FindByUserId.QUERY),
+        @NamedQuery(name = Meal.FindByUserIdBetweenDates.NAME,
+                    query = Meal.FindByUserIdBetweenDates.QUERY),
+        @NamedQuery(name = Meal.DeleteByIdAndUserId.NAME,
+                    query = Meal.DeleteByIdAndUserId.QUERY)
+})
+@Table(name = "MEALS", uniqueConstraints = {
+        @UniqueConstraint(name = "meals_unique_user_datetime_idx",
+                            columnNames = {"user_id", "date_time"})
+})
 public class Meal extends BaseEntity {
+
+    public static final class DeleteByIdAndUserId {
+        public static final String NAME = "Meal.DeleteByIdAndUserId";
+        public static final String ID = "id";
+        public static final String USER_ID = "userId";
+        static final String QUERY = "DELETE FROM Meal meal" +
+                " WHERE meal.id = :" + ID +
+                " AND meal.user.id = :" + USER_ID;
+    }
+
+    public static final class FindByUserIdBetweenDates {
+        public static final String NAME = "Meal.FindByUserIdBetweenDates";
+        public static final String START_DATE = "startDate";
+        public static final String END_DATE = "endDate";
+        public static final String USER_ID = "userId";
+        static final String QUERY = "SELECT meal FROM Meal meal" +
+                " WHERE meal.user.id = :" + USER_ID +
+                " AND meal.dateTime between :" + START_DATE + "" +
+                " AND :" + END_DATE +
+                " ORDER BY meal.dateTime DESC";
+    }
+
+    public static final class FindByUserId {
+        public static final String NAME = "Meal.FindByUserId";
+        public static final String USER_ID = "userId";
+        static final String QUERY = "SELECT meal FROM Meal meal" +
+                " WHERE meal.user.id = :" + USER_ID +
+                " ORDER BY meal.dateTime DESC";
+    }
+
+    public static final class FindByIdAndUserId {
+        public static final String NAME = "Meal.FindByIdAndUserId";
+        public static final String ID = "id";
+        public static final String USER_ID = "userId";
+        static final String QUERY = "SELECT meal FROM Meal meal" +
+                " WHERE meal.id = :" + ID + "" +
+                " AND meal.user.id = :" + USER_ID;
+    }
+
+    @NotNull
+    @Column(name = "date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @NotBlank
+    @Column(nullable = false)
     private String description;
 
+    @NotNull
+    @Column(nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -1,37 +1,76 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepositoryImpl implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            User user = entityManager.getReference(User.class, userId);
+            meal.setUser(user);
+            entityManager.persist(meal);
+        } else {
+            if (meal.getUser() == null || meal.getUser().getId() != userId) {
+                return null;
+            }
+            return entityManager.merge(meal);
+        }
+        return meal;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return entityManager
+                .createNamedQuery(Meal.DeleteByIdAndUserId.NAME)
+                .setParameter(Meal.DeleteByIdAndUserId.ID, id)
+                .setParameter(Meal.DeleteByIdAndUserId.USER_ID, userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> meals = entityManager
+                .createNamedQuery(Meal.FindByIdAndUserId.NAME, Meal.class)
+                .setParameter(Meal.FindByIdAndUserId.ID, id)
+                .setParameter(Meal.FindByIdAndUserId.USER_ID, userId)
+                .getResultList();
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return entityManager
+                .createNamedQuery(Meal.FindByUserId.NAME, Meal.class)
+                .setParameter(Meal.FindByUserId.USER_ID, userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return entityManager
+                .createNamedQuery(Meal.FindByUserIdBetweenDates.NAME, Meal.class)
+                .setParameter(Meal.FindByUserIdBetweenDates.USER_ID, userId)
+                .setParameter(Meal.FindByUserIdBetweenDates.START_DATE, startDate)
+                .setParameter(Meal.FindByUserIdBetweenDates.END_DATE, endDate)
+                .getResultList();
     }
 }
